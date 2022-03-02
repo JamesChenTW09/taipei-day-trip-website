fb5063cf217585a7f0e8113790df7e38a3f4910d
James Chen <js1031222@gmail.com>
2022-03-02 18:04:52 +0800

    update first week works

--- data/database/Config.py
+++
@@ -1,9 +0,0 @@
-dbConfig = {"db_host" : "localhost",
-            "db_port" : 3306,
-            "db_user" : "root",
-            "db_password" :"!weHELP0721james",
-            "db_database" : "taipei_attractions",
-            "db_poolName" : "my_db_pool",
-            "db_poolSize" : 10
-            }
-        
--- data/database/database.py
+++ data/database/database.py
@@ -1,5 +1,5 @@
 from mysql.connector import pooling
-from .Config import dbConfig
+from ..Config import dbConfig
 
 
 
